383a6626f01d054fab1929ee5e763503db5edaf6
HUASONHUA <you@example.com>
2025-09-04 15:57:37 +0800

    modify. 格式調整

--- OGKM_Lib/sql/INSERT INTO_customers.sql
+++ OGKM_Lib/sql/INSERT INTO_customers.sql
@@ -2,13 +2,10 @@ SELECT * FROM ogk.customers;
 
 INSERT INTO customers
 (id, name, email, password, birthday, gernder)
-VALUES('A123456789','花一豪','flowertest1@gmail.com',123456,'2000-11-11','M');
-INSERT INTO customers 
-(`id`, `name`, `email`, `password`, `birthday`, `gernder`)
- VALUES ('A123546789', '花一豪', 'flowertest3@gmail.com', '123456', '1999/5/9', 'M');
-INSERT INTO customers 
-(`id`, `name`, `email`, `password`, `birthday`, `gernder`)
- VALUES ('A123546789', '花三豪', 'flowertest3@gmail.com', '123456', '1999/5/9', 'M');
+VALUES
+('A123456789','花一豪','flowertest1@gmail.com',123456,'2000-11-11','M'),
+('A123546789','花二豪','flowertest2@gmail.com','123456','1999/5/9','M'),
+('A123546789','花三豪','flowertest3@gmail.com','123456','1999/5/9','M');
 INSERT INTO customers 
 (id, name, email, password, birthday, gernder,address,phone,subscribed,blood_type)
  VALUES ('A178946789', '花四豪','flowertest4@gmail.com','123456', '1999/5/9','M','台北市123路456號14F','0987654321',true,'A');
@@ -18,5 +15,5 @@ INSERT INTO customers
  VALUES ('A178988789', '花五豪','flowertest4@gmail.com','123456', '1999/5/9','M','台北市123路456號14F','0987654321',false,'A');
  
  INSERT INTO customers 
-                  (id,     name,                email, password, birthday, gernder,subscribed)
+ (id, name, email, password, birthday, gernder,subscribed)
  VALUES ('A175588789', '花六豪','flowertest6@gmail.com','123456', '1999/5/9','M',false);
