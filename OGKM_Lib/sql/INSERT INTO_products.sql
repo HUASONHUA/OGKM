69b1b476259bc2e6371f385b6c5a69ba0a3bd955
HUASONHUA <you@example.com>
2025-11-09 18:44:21 +0800

    feat. 刪除無用檔案

--- OGKM_Lib/sql/INSERT INTO_products.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO products(id, name, unitPrice, stock, photoUrl, description, shelfDate, discount, Sales)
-VALUES();
--- OGKM_Lib/sql/SELECT_order_status_log.sql
+++
@@ -1,2 +0,0 @@
-SELECT order_id, old_status, new_status, old_payment_note, 
-	new_payment_note, update_time FROM order_status_log WHERE order_id='2';
